4b972150542cd1325152261612d891e978232eae
meetgami510 <meetgami105@gmail.com>
2024-01-01 16:19:42 +0530

    feat: Dashboard Income,Expense and balance added

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
